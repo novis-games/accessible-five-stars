1cfafa3b212aa378cadfd112bc15d645e2c813fd
novis-games <info@novisgames.com>
2021-11-22 19:45:52 +0100

    Dynamica component & outputValues for binding

--- index.js
+++ index.js
@@ -1,52 +1,68 @@
-
-function createRating(objects, starNumber, ref, document) {
+var outputValues = []; 
+function createRating(objects, starNumber, document, div) {
     if (document) {
-        htmlOut = document.createElement("div");
-        var i = 1;
-        for (obj in objects) {
+        for (i=0; i < objects.length; i++ ) {
+            const obj = objects[i]; 
             const form = document.createElement("form");
             form.id = "accessible-form-" + i;
             const fieldset = document.createElement("fieldset");
-            fieldset.id = ref + "rating";
+            fieldset.id = obj.ref + "-rating";
+            fieldset.className="starability-slot"
             const legend = document.createElement("legend");
             legend.style.width = "max-content";
-            legend.textContent = "Punteggio " + ref + " (votate:" + obj + " su " + starNumber + " stelle):"
+            legend.textContent = "Punteggio " + obj.ref + " (votate: " + obj.value + " su " + starNumber + " stelle):"
+
             const radioNoRate = document.createElement("input");
             radioNoRate.type = "radio";
-            radioNoRate.id = "no-rate" + ref;
+            radioNoRate.id = "no-rate" + obj.ref;
             radioNoRate.className = "input-no-rate";
             radioNoRate.name = "rating";
             radioNoRate.value = "0"
             radioNoRate.checked = "true";
             radioNoRate.ariaLabel = "Non votato";
 
+            fieldset.appendChild(legend)
             fieldset.appendChild(radioNoRate);
+
 
 
             for (cont = 1; cont < starNumber + 1; cont++) {
                 const radio = document.createElement("input");
                 radio.type = "radio";
-                radio.id = ref + "-rate" + cont;
-                radio.className = "input-no-rate";
+                radio.id = obj.ref + "-rate" + cont;
                 radio.name = "rating";
                 radio.value = cont;
-                radio.onclick = function () { objects[i] = cont; };
-                if (objects[i] == cont) { radio.checked = "true" };
+                radio.addEventListener("click", function(){
+                var json = {}; 
+                delete json[obj.objName];
+                json[obj.objName] = radio.value; 
+                for (ix = 0; ix < outputValues.length; ix++)
+                {
+                    var jsonTemp = outputValues[ix]; 
+                    if (jsonTemp && jsonTemp[obj.objName])
+                    {
+                        outputValues.splice(ix, 1); 
+                    }
+                }
+                outputValues.push(json); 
+                legend.textContent = "Punteggio " + obj.ref + " (votate: " + radio.value + " su " + starNumber + " stelle):" }, false);
+                if (objects[i].value == cont) { radio.checked = "true" };
 
                 const label = document.createElement("label");
                 label.htmlFor = radio.id;
-                label.title = cont + "stelle";
+                label.title = cont + " stelle";
                 label.innerText = label.title;
                 fieldset.appendChild(radio)
                 fieldset.appendChild(label);
 
             }
             form.appendChild(fieldset);
-            htmlOut.appendChild(form);
-            document.appendChild(htmlOut); 
-            i++;
+            div.appendChild(form);
         }
     }
 };
 
+
+
 exports.createRating = createRating;
+exports.outputValues = outputValues;
