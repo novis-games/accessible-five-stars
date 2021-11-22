51050628e3e460078bc9b21bfdb668b0b254aba3
novis-games <info@novisgames.com>
2021-11-22 15:21:08 +0100

    super

--- star-rating.js
+++ star-rating.js
@@ -2,20 +2,20 @@ var htmlOut;
 
 function createRating(objects, starNumber, ref)
 {
-    htmlOut = document.createElement("div");
+    htmlOut = super.documentcreateElement("div");
     console.log("HTML OUT")
 
     var i = 1; 
 for (obj in objects)
 {
-    const form = document.createElement("form"); 
+    const form = super.documentcreateElement("form"); 
     form.id = "accessible-form-" + i;
-    const fieldset = document.createElement("fieldset");
+    const fieldset = super.documentcreateElement("fieldset");
     fieldset.id=ref+"rating"; 
-    const legend = document.createElement("legend"); 
+    const legend = super.documentcreateElement("legend"); 
     legend.style.width = "max-content"; 
     legend.textContent = "Punteggio " +ref+ " (votate:"+obj+" su " + starNumber + " stelle):"
-    const radioNoRate = document.createElement("input"); 
+    const radioNoRate = super.documentcreateElement("input"); 
     radioNoRate.type= "radio"; 
     radioNoRate.id="no-rate"+ref; 
     radioNoRate.className = "input-no-rate"; 
@@ -29,7 +29,7 @@ for (obj in objects)
 
     for (cont = 1; cont < starNumber +1; cont++ )
     {
-        const radio = document.createElement("input"); 
+        const radio = super.documentcreateElement("input"); 
         radio.type= "radio"; 
         radio.id=ref+"-rate"+cont; 
         radio.className = "input-no-rate"; 
@@ -39,7 +39,7 @@ for (obj in objects)
         if (objects[i]==cont)
         {radio.checked="true"}; 
 
-        const label = document.createElement("label"); 
+        const label = super.documentcreateElement("label"); 
         label.htmlFor=radio.id; 
         label.title = cont + "stelle"; 
         label.innerText= label.title; 
