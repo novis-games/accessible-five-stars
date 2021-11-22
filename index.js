2b8baf980c114271bb0fee59d876b9fa2e6b773b
novis-games <info@novisgames.com>
2021-11-22 14:58:54 +0100

    modifiche plugin

--- index.js
+++ index.js
@@ -1,54 +1,9 @@
 
 
-function createTable(objects, starNumber, ref)
-{
-    var i = 1; 
-for (obj in objects)
-{
-    const form = document.createElement("form"); 
-    form.id = "accessible-form-" + i;
-    const fieldset = document.createElement("div");
-    fieldset.id=ref+"rating"; 
-    const legend = document.createElement("legend"); 
-    legend.style.width = "max-content"; 
-    legend.textContent = "Punteggio " +ref+ " (votate:"+obj+" su " + starNumber + " stelle):"
-    const radioNoRate = document.createElement("input"); 
-    radioNoRate.type= "radio"; 
-    radioNoRate.id="no-rate"+ref; 
-    radioNoRate.className = "input-no-rate"; 
-    radioNoRate.name="rating"; 
-    radioNoRate.value="0"
-    radioNoRate.checked = "true"; 
-    radioNoRate.ariaLabel = "Non votato"; 
-
-    fieldset.appendChild(radioNoRate); 
 
 
-    for (cont = 1; cont < starNumber +1; cont++ )
-    {
-        const radio = document.createElement("input"); 
-        radio.type= "radio"; 
-        radio.id=ref+"-rate"+cont; 
-        radio.className = "input-no-rate"; 
-        radio.name="rating"; 
-        radio.value=cont; 
-        radio.onclick=function () {objects[i]=cont;}; 
-        if (objects[i]==cont)
-        {radio.checked="true"}; 
-
-        const label = document.createElement("label"); 
-        label.htmlFor=radio.id; 
-        label.title = cont + "stelle"; 
-        label.innerText= label.title; 
-        fieldset.appendChild(radio)
-        fieldset.appendChild(label); 
-      
-    }
-    form.appendChild(fieldset); 
-    document.appendChild(form); 
-    i++; 
-}
-}; 
+const starRating = require('./star-rating');
+console.log(starRating.createRating()); 
 
 /*<form>
 <fieldset id="accessibility-rating" class="starability-slot">
@@ -63,6 +18,7 @@ for (obj in objects)
     name="rating"
     value="0"
     checked
+
     aria-label="Non votato"
   />
   <input
